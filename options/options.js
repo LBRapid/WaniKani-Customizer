4ead5edb7209df23ae17985bb8f6f89a3a6afb89
Kyle Coburn <kylecoburn@gmail.com>
2013-12-14 12:48:38 -0800

    Rename options

--- options/options.js
+++ options/options.js
@@ -1,13 +1,16 @@
+var isChromeExtension = typeof chrome != 'undefined';
+
 var option_groups = [
 	[
-		['Redirect WaniKani pages to reviews and lessons', 'radio'],
-		['Never', '-1', 'redirect'],
-		['When first available', '0', 'redirect']
+		['Redirect WaniKani pages to reviews and lessons', 'checkbox'],
+		['When first available', 'redirect_i', 'check-i']
 	],
 	[
 		['Reviews & Lessons', 'checkbox'],
-		['Sort Radicals to the front of the queue', 'sort_rad', 'check-i'],
-		['Differentiate meanings and on/kun readings', 'disable_custom_reviews', 'check-i']
+		['Sort current level Radicals to the front of the queue', 'sort_rad_i', 'check-i'],
+		['Sort current level Kanji to the front of the queue', 'sort_kan', 'check'],
+		// ['Sort Burned items to the front of the queue', 'sort_burn_i', 'check-i'],
+		['Differentiate meanings and on/kun readings', 'disable_custom_reviews_i', 'check-i']
 	],
 	[
 		['Tofugu Services', 'checkbox'],
@@ -15,18 +18,18 @@ var option_groups = [
 		['EtoEto user', 'etoeto', 'check'],
 	],
 	[
-		['Userscripts', 'checkbox', 'Additional options created by the WaniKani community'],
-		['Collapsable nested quotes on the forums', 'no_collapse', 'check-i', ['pankeki', '2752']]
+		['Miscellaneous', 'checkbox'],
+		['24 hour format', '12_hours_i', 'check-i'],
 	],
 	[
-		['Miscellaneous', 'checkbox'],
-		['24 hour format', '12_hours', 'check-i'],
+		['Userscripts', 'checkbox', 'Additional options created by the WaniKani community'],
+		['Collapsable nested quotes on the forums', 'no_collapse_i', 'check-i', ['pankeki', '2752']]
 	]
 ];
 
 
 /* SAVE */
-var saveTimer, savedSucessfully;
+var saveTimer, savedSucessfully, saved_options;
 
 function waitToCancel() {
 	if (saveTimer)
@@ -45,17 +48,26 @@ function waitToCancel() {
 }
 
 function saveOption(theKey, theValue) {
-	savedSucessfully = false;
 	var status = document.getElementById("status");
-	status.innerHTML = "Saving...";
-	status.className = "saving";
-	var syncValue = {};
-	syncValue[theKey] = theValue;
-	chrome.storage.sync.set(syncValue, function() {
+	if (isChromeExtension) {
+		savedSucessfully = false;
+		status.innerHTML = "Saving...";
+		status.className = "saving";
+		var syncValue = {};
+		syncValue[theKey] = theValue;
+		chrome.storage.sync.set(syncValue, function() {
+			status.innerHTML = "Saved.";
+			status.className = "saved";
+			savedSucessfully = true;
+		});
+	} else {
+		saved_options[theKey] = theValue;
+		console.log(saved_options);
+		localStorage.setItem('wk_options', JSON.stringify(saved_options));
 		status.innerHTML = "Saved.";
 		status.className = "saved";
 		savedSucessfully = true;
-	});
+	}
 	waitToCancel();
 }
 
@@ -64,15 +76,15 @@ function saveOption(theKey, theValue) {
 document.addEventListener('DOMContentLoaded', function() {
 	var html_str = '';
 	for (var i = 0; i < option_groups.length; ++i) {
-		var options = option_groups[i];
-		var metadata = options[0];
+		var optionset = option_groups[i];
+		var metadata = optionset[0];
 		var inputType = metadata[1];
 		var description = metadata[2];
 		html_str += '<div class="option-set"><h3>' + metadata[0] + '</h3>';
 		if (description)
 			html_str += '<em>'+description+'</em>';
-		for (var j = 1; j < options.length; ++j) {
-			var option = options[j];
+		for (var j = 1; j < optionset.length; ++j) {
+			var option = optionset[j];
 			html_str += '<p><label><input type="'+inputType+'" class="'+option[2]+'" name="'+option[2]+'" value="'+option[1]+'" /> ' + option[0] + '</label>';
 			var author = option[3];
 			if (author)
@@ -83,6 +95,15 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 	document.getElementById('options').innerHTML = html_str;
 
+	// OPTIONS
+	function saveEventValue(event) {
+		saveOption(event.target.value, event.target.checked);
+	}
+
+	function saveEventInverted(event) {
+		saveOption(event.target.value, !event.target.checked);
+	}
+
 	var customBoxes = document.getElementsByClassName('check');
 	for (var i = 0; i < customBoxes.length; i++) {
 		customBoxes[i].addEventListener('click', saveEventValue);
@@ -91,23 +112,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	for (var i = 0; i < invertedBoxes.length; i++) {
 		invertedBoxes[i].addEventListener('click', saveEventInverted);
 	}
-	var radioItems = document.getElementsByClassName('redirect');
-	for (var i = 0; i < radioItems.length; i++) {
-		radioItems[i].addEventListener('click', saveInteger);
-	}
 
-	chrome.storage.sync.get(null, function(items) {
-		var prevLevel = items['redirect'];
-		if (!prevLevel) {
-			prevLevel = 0;
-		}
-		for (var i = 0; i < radioItems.length; i++) {
-			var radItem = radioItems[i];
-			if (parseInt(radItem.value) == prevLevel) {
-				radItem.checked = true;
-				break;
-			}
-		}
+	function initialize(items) {
 		for (var i = 0; i < customBoxes.length; i++) {
 			var customItem = customBoxes[i];
 			if (items[customItem.value])
@@ -118,20 +124,19 @@ document.addEventListener('DOMContentLoaded', function() {
 			if (!items[customItem.value])
 				customItem.checked = true;
 		}
-	});
+	}
+
+	if (isChromeExtension) {
+		chrome.storage.sync.get(null, function(items) {
+			initialize(items);
+		});
+	} else {
+		var items = localStorage.getItem('wk_options');
+		console.log(items);
+		if (items)
+			saved_options = JSON.parse(items);
+		if (!saved_options)
+			saved_options = {};
+		initialize(saved_options);
+	}
 });
-
-
-/* OPTION EVENTS */
-function saveInteger(event) {
-	var newLevel = parseInt(event.target.value);
-	saveOption(event.target.name, newLevel);
-}
-
-function saveEventValue(event) {
-	saveOption(event.target.value, event.target.checked);
-}
-
-function saveEventInverted(event) {
-	saveOption(event.target.value, !event.target.checked);
-}
