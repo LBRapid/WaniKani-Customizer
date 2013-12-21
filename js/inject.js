478633f27e474bbf1ce09216bd5da4ce248f7996
Kyle Coburn <kylecoburn@gmail.com>
2013-12-21 13:52:37 -0800

    Update review sorting

--- js/inject.js
+++ js/inject.js
@@ -1,5 +1,3 @@
-var listening = true;
-
 if (!window.$) {
 	for (var key in window) {
 		if (key.substr(0, 6).toLowerCase() == 'jquery') {
@@ -10,7 +8,9 @@ if (!window.$) {
 }
 
 if (window.$) {
+	var listening = true, initialized = false;
 	var options, radData, kanData, prevItem, bookmarks;
+	var prevCount = 0;
 
 	function updateBookmark(thread, page) {
 		bookmarks[thread] = page;
@@ -19,10 +19,10 @@ if (window.$) {
 
 	function levelIndex(item, currRad, currKan) {
 		if (item.rad) {
-			if (currRad && currRad.indexOf(item.en[0]) !== -1)
+			if (currRad && radData.indexOf(item.en[0]) !== -1)
 				return 0;
-		} else if (item.kan) {
-			if (currKan && currKan.indexOf(item.kan) !== -1)
+		} else if (currKan && item.kan) {
+			if (kanData.indexOf(item.kan) !== -1)
 				return 1;
 		}
 		return 2;
@@ -35,7 +35,8 @@ if (window.$) {
 		listening = false;
 		setTimeout(function() {
 			listening = true;
-		}, 100)
+		}, 100);
+
 		var sortRad = !options.sort_rad_i, sortKan = options.sort_kan;
 		if (!sortRad && !sortKan)
 			return;
@@ -88,61 +89,63 @@ if (window.$) {
 		$.jStorage.set('l/lessonQueue', saveForLater);
 	}
 
-	function setReviewQueue(needsUpdating) {
-		var currRad = !options.sort_rad_i ? localStorage.getItem('radicalData') : null;
-		var currKan = options.sort_kan ? localStorage.getItem('kanjiData') : null;
-		var storedReviews = $.jStorage.get("reviewQueue");
-		var checkBurn = false; // !options.sort_burn_i; TODO fix
-		if (needsUpdating !== true) {
-			needsUpdating = false;
-			for (var i = 0; i < storedReviews.length; ++i) {
-				var item = storedReviews[i];
-				if (!item)
-					continue;
-				if ((checkBurn && item.srs == 8) || levelIndex(item, currRad, currKan) !== 2) {
-					needsUpdating = true;
-					break;
-				}
-			}
-		}
-		if (!needsUpdating)
+	function setReviewQueue() {
+		if (initialized && !listening)
 			return;
+		listening = false;
+		setTimeout(function() {
+			listening = true;
+		}, 1000);
+
+		var checkRad = !options.sort_rad_i, checkKan = options.sort_kan, checkBurn = options.sort_burn;
+		var storedReviews = $.jStorage.get('reviewQueue').concat($.jStorage.get('activeQueue'));
+		var needsUpdating;
 
 		var itemsArrays = [[], [], []];
-		var activeArr = $.jStorage.get('activeQueue');
-		if (!activeArr)
-			return;
-		storedReviews = storedReviews.concat(activeArr);
 		var currCount = 0, burnCount = 0;
 		for (var i = 0; i < storedReviews.length; ++i) {
 			var item = storedReviews[i];
 			if (item) {
-				var li;
-				if (checkBurn && item.srs == 8) {
-					li = 1;
+				var li = 2;
+				if (item.srs == 8) {
+					if (checkBurn && burnCount < 10)
+						li = 1;
 					++burnCount;
 				} else {
-					li = levelIndex(item, currRad, null);
+					li = levelIndex(item, checkRad, checkKan);
 					if (li < 2)
 						++currCount;
 				}
 				itemsArrays[li].push(item);
 			}
 		}
-		var actQueue = itemsArrays[0].concat(itemsArrays[1]);
-		$.jStorage.set('activeQueue', actQueue);
-		$.jStorage.set('reviewQueue', itemsArrays[2]);
-		$('#question #current-count').text(currCount);
-		$('#question #burn-count').text(burnCount);
+
+		var actQueue = itemsArrays[0];
+		var laterQueue = itemsArrays[2];
+		var needsUpdating = true;
+		if (actQueue.length == 0) {
+			actQueue = itemsArrays[1];
+			if (actQueue.length == 0)
+				needsUpdating = false;
+		} else {
+			laterQueue.concat(itemsArrays[1]);
+		}
+		if (needsUpdating) {
+			$.jStorage.set('activeQueue', actQueue);
+			$.jStorage.set('reviewQueue', laterQueue);
+			$.jStorage.set('currentItem', actQueue[0]);
+			initialized = true;
+		}
+		if (currCount > 0)
+			$('#question #current-count').text(currCount);
+		if (burnCount > 0)
+			$('#question #burn-count').text(burnCount);
 		$('#question #current-stats').toggle(currCount > 0);
 		$('#question #burn-stats').toggle(burnCount > 0);
 	}
 
 	if ($.jStorage && document.getElementById('reviews')) {
-		var kunyomi_list = [
-			"売","草","戸","糸","皿","竹","皮","花","耳","又","雪","打","村","貝","赤","夏","兄","広","父","冬","久","谷","川","田","目","夕","付","左","手","合","四","犬","引","玉","申","々","投","雲","森","朝","紙","首","何","雨","色","里","羽","池","黄","星","麦","母","丸","見","右","虫","石","林","氷","考","飲","泳","顔","軽","葉","頭","起","読","横","追","島","待","受","指","拾","伝","仲","寒","湯","歯","昔","始","持","乗","物","良","声","方","鳴","答","橋","緑","鏡","様","悲","祭","暑","着","皆","場","買","泣","猫","秋","帰","晴","昼","春","浅","寺","岩","坂","浜","芽","甘","替","奪","磨","貸","埼","滝","翔","懐","遠","遊","荒","俺","鼻","塩","熊","戻","梅","箱","荷","喜","笑","取","浴","若","舌","胸","忙","困","底","臭","穴","岡","尻","浮","焼","株","渡","宮","違","狭","届","腰","肩","値","割","傘","側","呼","抜","針","掛","城","細","厚","藤","靴","娘","江","詰","渇","押","枕","悩","酢","縦","幾","亀","彼","卵","飼","込","机","窓","灰","汗","杉","爪","裏","巣","捨","鍋","払","埋","吐","芋","恥","豚","緩","寝","抱","繰","伸","津","幅","沖","扱","宜","控","壁","片","稲","駆","吹","堀","桃","瀬","蛍","虎","蜂","桜","枠","賭","髪","梨","伺","畑","涙","堅","綱","潟","誰","笠","嵐","姫","謎","刃","侍","曇","呪","也","頃","舟","肌","棚","釣","腕","芝","袋","柄","琴","沼","垣","叱",
-			"墨","癖","吾","鍵","猿","箸","霧","唇","虹","爽","脇","咲","踊","闇","霜","蚊","貼","蛇","煮","駒","桑","鰐","蟹","淀"
-		];
+		var kunyomi_list = {"売":1,"草":1,"戸":1,"糸":1,"皿":1,"竹":1,"皮":1,"花":1,"耳":1,"又":1,"雪":1,"打":1,"村":1,"貝":1,"赤":1,"夏":1,"兄":1,"広":1,"父":1,"冬":1,"久":1,"谷":1,"川":1,"田":1,"目":1,"夕":1,"付":1,"左":1,"手":1,"合":1,"四":1,"犬":1,"引":1,"玉":1,"申":1,"々":1,"投":1,"雲":1,"森":1,"朝":1,"紙":1,"首":1,"何":1,"雨":1,"色":1,"里":1,"羽":1,"池":1,"黄":1,"星":1,"麦":1,"母":1,"丸":1,"見":1,"右":1,"虫":1,"石":1,"林":1,"氷":1,"考":1,"飲":1,"泳":1,"顔":1,"軽":1,"葉":1,"頭":1,"起":1,"読":1,"横":1,"追":1,"島":1,"待":1,"受":1,"指":1,"拾":1,"伝":1,"仲":1,"寒":1,"湯":1,"歯":1,"昔":1,"始":1,"持":1,"乗":1,"物":1,"良":1,"声":1,"方":1,"鳴":1,"答":1,"橋":1,"緑":1,"鏡":1,"様":1,"悲":1,"祭":1,"暑":1,"着":1,"皆":1,"場":1,"買":1,"泣":1,"猫":1,"秋":1,"帰":1,"晴":1,"昼":1,"春":1,"浅":1,"寺":1,"岩":1,"坂":1,"浜":1,"芽":1,"甘":1,"替":1,"奪":1,"磨":1,"貸":1,"埼":1,"滝":1,"翔":1,"懐":1,"遠":1,"遊":1,"荒":1,"俺":1,"鼻":1,"塩":1,"熊":1,"戻":1,"梅":1,"箱":1,"荷":1,"喜":1,"笑":1,"取":1,"浴":1,"若":1,"舌":1,"胸":1,"忙":1,"困":1,"底":1,"臭":1,"穴":1,"岡":1,"尻":1,"浮":1,"焼":1,"株":1,"渡":1,"宮":1,"違":1,"狭":1,"届":1,"腰":1,"肩":1,"値":1,"割":1,"傘":1,"側":1,"呼":1,"抜":1,"針":1,"掛":1,"城":1,"細":1,"厚":1,"藤":1,"靴":1,"娘":1,"江":1,"詰":1,"渇":1,"押":1,"枕":1,"悩":1,"酢":1,"縦":1,"幾":1,"亀":1,"彼":1,"卵":1,"飼":1,"込":1,"机":1,"窓":1,"灰":1,"汗":1,"杉":1,"爪":1,"裏":1,"巣":1,"捨":1,"鍋":1,"払":1,"埋":1,"吐":1,"芋":1,"恥":1,"豚":1,"緩":1,"寝":1,"抱":1,"繰":1,"伸":1,"津":1,"幅":1,"沖":1,"扱":1,"宜":1,"控":1,"壁":1,"片":1,"稲":1,"駆":1,"吹":1,"堀":1,"桃":1,"瀬":1,"蛍":1,"虎":1,"蜂":1,"桜":1,"枠":1,"賭":1,"髪":1,"梨":1,"伺":1,"畑":1,"涙":1,"堅":1,"綱":1,"潟":1,"誰":1,"笠":1,"嵐":1,"姫":1,"謎":1,"刃":1,"侍":1,"曇":1,"呪":1,"也":1,"頃":1,"舟":1,"肌":1,"棚":1,"釣":1,"腕":1,"芝":1,"袋":1,"柄":1,"琴":1,"沼":1,"垣":1,"叱":1,"墨":1,"癖":1,"吾":1,"鍵":1,"猿":1,"箸":1,"霧":1,"唇":1,"虹":1,"爽":1,"脇":1,"咲":1,"踊":1,"闇":1,"霜":1,"蚊":1,"貼":1,"蛇":1,"煮":1,"駒":1,"桑":1,"鰐":1,"蟹":1,"淀":1};
 		radData = localStorage.getItem('radicalData');
 		kanData = localStorage.getItem('kanjiData');
 
@@ -158,7 +161,7 @@ if (window.$) {
 					$('#question #burn-stats').hide();
 				}
 			} else {
-				var lIdx = levelIndex(prevItem, radData, kanData);
+				var lIdx = levelIndex(prevItem, true, true);
 				if (lIdx < 2) {
 					var currText = $('#question #current-count');
 					var currValue = parseInt(currText.text());
@@ -176,7 +179,15 @@ if (window.$) {
 			if (!item)
 				return;
 			var burning = item.srs == 8;
-		    var lIdx = levelIndex(item, radData, kanData);
+			var lIdx = levelIndex(item, true, true);
+			var newCount = $('#question #available-count').text();
+			if (!burning && lIdx == 2 && newCount > prevCount) {
+				initialized = true;
+				setReviewQueue();
+				prevCount = newCount;
+				return;
+			}
+			prevCount = newCount;
 			$('.icon-chevron-right').toggleClass('icon-burn burning', burning);
 			$('.icon-chevron-right').toggleClass('icon-level active-level', !burning && lIdx !== 2);
 			if (lIdx === 0) {
@@ -191,13 +202,7 @@ if (window.$) {
 				setTimeout(function() {
 					var quest = $('#question-type.reading');
 					if (quest.length !== 0) {
-						var found = 'On';
-						for (var i = 0; i < kunyomi_list.length; i++) {
-						    if (kunyomi_list[i] === kChar) {
-						        found = 'Kun';
-						        break;
-						    }
-						}
+						var found = kunyomi_list[kChar] ? 'Kun' : 'On';
 						quest.html('<h1><strong>' + found + "'yomi</strong> Reading?</h1>");
 					}
 				}, 0);
@@ -214,7 +219,6 @@ if (window.$) {
 		} else {
 			options = {};
 		}
-		var initialized = false;
 		if ($.jStorage) {
 			if (document.getElementById('lessons')) {
 				setTimeout(function() {
@@ -233,9 +237,10 @@ if (window.$) {
 				return;
 			}
 			if (document.getElementById('reviews')) {
-				setTimeout(function() { setReviewQueue(true); }, 0);
-				setTimeout(function() { setReviewQueue(true); }, 5000);
-				setInterval(setReviewQueue, 60000);
+				setTimeout(function() {
+					if (!initialized)
+						setReviewQueue();
+				}, 1000);
 				return;
 			}
 		}
@@ -301,28 +306,26 @@ if (window.$) {
 		var itemCount = $('.navbar-static-top li.lessons a span').text();
 		if (itemCount) {
 			if (itemCount != '0') {
-				var loadLessons = options.redirect_i;
-				if (!loadLessons) {
-					if (!localStorage.getItem('lessonsFound')) {
-						loadLessons = true;
+				if (options.redirect) {
+					if (localStorage.getItem('lessonsFound')) {
+						loadLessons = false;
+					} else {
 						localStorage.setItem('lessonsFound', true);
+						location.href = '/lesson/session';
+						return;
 					}
-				}
-				if (loadLessons) {
-					window.location.pathname = '/lesson/session';
-					return;
 				}
 			} else {
 				localStorage.setItem('lessonsFound', false);
 			}
 		}
-		if (!options.redirect_i) {
+		if (options.redirect) {
 			var nextTimeDisplay = $('.review-status li.next time');
 			if (nextTimeDisplay.length > 0) {
 				var diff = nextTimeDisplay.attr('datetime') - new Date().getTime() / 1000;
 				if (diff > 30) {
 					setTimeout(function() {
-						window.location.pathname = '/review/session';
+						location.href = '/review/session';
 					}, Math.ceil(diff * 1000) + 1000);
 					return;
 				}
